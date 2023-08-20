bc11366d3a0b5249a9e8048071234086add94142
ryosuke-shibata-hub <ibis.stk@gmail.com>
2023-08-21 00:14:29 +0900

    お天気アプリ完

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,13 +1,49 @@
 import React from 'react';
 import Title from './components/Title';
 import Form from "./components/Form";
+import Results from "./components/Results";
 import './App.css';
+import { useState } from "react";
 
+type ResultsStateType = {
+    country: string,
+    cityName: string,
+    temperature: string,
+    conditionText: string,
+    icon: string,
+}
 function App() {
+    //データは全てstate内に保存する
+    const [city, setCity] = useState<string>("");
+    const [results, setResults] = useState<ResultsStateType>({
+        country: "",
+        cityName: "",
+        temperature: "",
+        conditionText: "",
+        icon: "",
+    });
+    //Form.tsxからApiに都市名を渡す
+    const getWeather = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        fetch(`https://api.weatherapi.com/v1/current.json?key=053b5e37ce6548dda1f134204232008&q=${city}&aqi=no`)
+            .then(res => res.json())
+            .then(data => {
+                setResults({
+                    country: data.location.country,
+                    cityName: data.location.name,
+                    temperature: data.current.temp_c,
+                    conditionText: data.current.condition.text,
+                    icon: data.current.condition.icon,
+                })
+            })
+    }
     return (
-        <div className="test">
-            <Title />
-            <Form />
+        <div className="wrapper">
+            <div className='container'>
+                <Title />
+                <Form setCity={setCity} getWeather={getWeather} />
+                <Results results={results} />
+            </div>
         </div>
     );
 }
---
+++ app/src/components/Results.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { useState } from "react";
+
+type ResultPropsType = {
+    results: {
+        country: string,
+        cityName: string,
+        temperature: string,
+        conditionText: string,
+        icon: string,
+    }
+}
+const Results = (props: ResultPropsType) => {
+    return (
+        <div>
+            {props.results.country &&
+                <div className="results-country">{props.results.country}</div>
+            }
+            {props.results.cityName &&
+                <div className="results-city">{props.results.cityName}</div>
+            }
+            {props.results.temperature &&
+                <div className="results-temperature">{props.results.temperature}</div>
+            }
+            {props.results.conditionText &&
+                <div className="results-condition">
+                    <img src={props.results.icon} alt="icon" />
+                    <span>{props.results.conditionText}</span>
+                </div>
+            }
+        </div>
+    );
+};
+
+
+export default Results;
